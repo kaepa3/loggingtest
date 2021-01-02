36e003872c72e3ff23ddb60aa24c0fe9940ab5e2
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2021-01-02 21:26:06 +0900

    add count script

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
+	"math/rand"
 	"os"
+	"strconv"
 
 	"github.com/op/go-logging"
 )
@@ -13,11 +16,12 @@ var log = logging.MustGetLogger("example")
 var sc = bufio.NewScanner(os.Stdin)
 
 var format = logging.MustStringFormatter(
-	`%{time:15:04:05.000} %{shortfunc} ▶ %{level:.4s} %{message}`,
+	`time:%{time:2006/01/02-15:04:05.000}	func:%{shortfunc}	level:%{level:.4s}	msg:%{message}`,
 )
 
 func main() {
 	fmt.Println("start")
+
 	f := OpenFile("log.log")
 	defer f.Close()
 	backend := logging.NewLogBackend(f, "", 0)
@@ -26,22 +30,60 @@ func main() {
 
 	flg := true
 	for flg {
-		if sc.Scan() {
-			t := sc.Text()
-			switch t {
-			case "e":
-				flg = false
-			default:
+		flg = process(flg)
+	}
+}
+func process(flg bool) bool {
+	if sc.Scan() {
+		t := sc.Text()
+
+		switch t {
+		case "e":
+			return false
+		default:
+			if count, e := strconv.Atoi(t); e == nil {
+				for i := 0; i < count; i++ {
+					if text, err := RandomString(rand.Intn(8) + 4); err == nil {
+						fmt.Println(text)
+						switch rand.Intn(3) {
+						case 0:
+							log.Error(text)
+						case 1:
+							log.Debug(text)
+						case 2:
+							log.Info(text)
+						default:
+							log.Warning(text)
+						}
+					}
+				}
+			} else {
 				if len(t) < 10 {
 					log.Debug(t)
 				} else {
 					log.Info(t)
 				}
 			}
-		} else {
-			break
 		}
 	}
+	return true
+}
+func RandomString(digit int) (string, error) {
+	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+	// 乱数を生成
+	b := make([]byte, digit)
+	if _, err := rand.Read(b); err != nil {
+		return "", errors.New("unexpected error...")
+	}
+
+	// letters からランダムに取り出して文字列を生成
+	var result string
+	for _, v := range b {
+		// index が letters の長さに収まるように調整
+		result += string(letters[int(v)%len(letters)])
+	}
+	return result, nil
 }
 
 func OpenFile(path string) *os.File {
